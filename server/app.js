ab293714d6ec7d020704116b1e9b052ad88bf100
Bianca Casetta <biancacasetta@gmail.com>
2025-05-26 23:42:56 +0200

    added router and middlewares to server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
+import { router } from './router.js';
 
 // create server
 const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(express.static("../client/dist"));
+app.use(router);
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
