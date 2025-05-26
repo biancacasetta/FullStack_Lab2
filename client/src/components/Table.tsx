939e6d488afa2e16bbc11b1d87e9041f48553d5a
Bianca Casetta <biancacasetta@gmail.com>
2025-05-26 23:46:00 +0200

    added table component using tanstack

---
+++ client/src/components/Table.tsx
@@ -0,0 +1,91 @@
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+} from '@tanstack/react-table';
+
+import { Assignment } from '../types';
+import { useState } from 'react';
+
+type Props = {
+    data: Assignment[];
+}
+
+const Table: React.FC<Props> = ({ data }) => {
+    const [sorting, setSorting] = useState<SortingState>([]);
+
+    const columnHelper = createColumnHelper<Assignment>()
+
+    const columns = [
+    columnHelper.accessor('employee_id', {
+            header: "Employee ID",
+            cell: info => info.getValue(),
+            enableSorting: true
+        }),
+        columnHelper.accessor('employee_name', {
+            header: "Employee Name",
+            cell: info => info.getValue(),
+            enableSorting: true
+        }),
+        columnHelper.accessor('project_name', {
+            header: "Project Name",
+            cell: info => info.getValue(),
+            enableSorting: true
+        }),
+        columnHelper.accessor('start_date', {
+            header: "Start Date",
+            cell: info => info.getValue(),
+            enableSorting: true
+        })
+    ]
+
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        onSortingChange: setSorting,
+        state: {
+            sorting,
+        }
+    });
+
+    return (
+        <div className="w-1/2">
+            <table className='w-full bg-purple-200 rounded-2xl overflow-hidden'>
+                <thead className='bg-purple-500'>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(h => (
+                                <th key={h.id} onClick={h.column.getToggleSortingHandler()}
+                                className='hover:cursor-pointer select-none p-2 w-1/4'>
+                                    {flexRender(h.column.columnDef.header, h.getContext())}
+                                    {{
+                                        asc: ' ⇧',
+                                        desc: ' ⇩'
+                                    }[h.column.getIsSorted() as string] ?? null}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody className='text-center'>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id} className='p-2'>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Table;
